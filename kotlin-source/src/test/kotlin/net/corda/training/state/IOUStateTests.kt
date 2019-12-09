a5c1ba1757cc637bbf8e821c7325f91e7b24d8a6
bobjac <bob.jacobs@outlook.com>
2019-12-09 16:25:34 -0500

    added hasPaidFieldOfCorrectType test

--- kotlin-source/src/test/kotlin/net/corda/training/state/IOUStateTests.kt
+++ kotlin-source/src/test/kotlin/net/corda/training/state/IOUStateTests.kt
@@ -79,13 +79,13 @@ class IOUStateTests {
      *   [IOUState.amount] property. You can create an instance of the [Amount] class that takes a zero value and a token
      *   representing the currency - which should be the same currency as the [IOUState.amount] property.
      */
-//    @Test
-//    fun hasPaidFieldOfCorrectType() {
-//        // Does the paid field exist?
-//        IOUState::class.java.getDeclaredField("paid")
-//        // Is the paid field of the correct type?
-//        assertEquals(IOUState::class.java.getDeclaredField("paid").type, Amount::class.java)
-//    }
+    @Test
+    fun hasPaidFieldOfCorrectType() {
+        // Does the paid field exist?
+        IOUState::class.java.getDeclaredField("paid")
+        // Is the paid field of the correct type?
+        assertEquals(IOUState::class.java.getDeclaredField("paid").type, Amount::class.java)
+    }
 
     /**
      * Task 5.
